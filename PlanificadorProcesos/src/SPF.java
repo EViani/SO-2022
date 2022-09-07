8b229b9b583a278681a385dc1495a5550626bf77
esteban <eviani94@hotmail.com>
2022-09-07 00:25:59 -0300

    agregado de SPF

--- PlanificadorProcesos/src/SPF.java
+++ PlanificadorProcesos/src/SPF.java
@@ -22,13 +22,18 @@ public class SPF {
 			lista.add(p);
 		}
 	}
+	
+	public void cargarTI(float ti) {
+		this.ti=ti;
+		
+	}
 
 	private ArrayList<Proceso> ordenar(float rel, ArrayList<Proceso> apro) {
 		Proceso proc;
 		
 		for(int i=0;i<lista.size();i++) {
 				for(int j=0;j<lista.size()-1;j++) {
-					if(apro.get(j).getTF()>apro.get(j+1).getTF() && reloj>=apro.get(j).getTLL() && reloj>=apro.get(j+1).getTLL()) {
+					if(apro.get(j).getTF()>apro.get(j+1).getTF() && rel>=apro.get(j).getTLL() && rel>=apro.get(j+1).getTLL()) {
 						proc=lista.get(j);
 						apro.set(j,apro.get(j+1));
 						apro.set(j+1, proc);
@@ -40,7 +45,7 @@ public class SPF {
 	}
 	
 	
-	public void procesar() {
+	private void procesar() {
 		char pant;
 		pant='$';
 		resetear();
@@ -54,21 +59,27 @@ public class SPF {
 			ordenar(r,lista);
 			for(int i=0;i<lista.size();i++) {
 				tf=lista.get(i).getTF();
-				if(tf>0) {
+				if(tf>0 && lista.get(i).getTLL()<=r) {
 					if(lista.get(i).getPID()!=pant && pant!='$') {
 						System.out.println("call planificador de corto plazo");
-						reloj+=ti/2;
+						reloj+=ti;
 					}
-					
-					
+					reloj+=lista.get(i).getTS();
+					lista.get(i).procesar(tf);
+					lista.get(i).calcularTR(reloj);
+					if(pant!= lista.get(i).getPID() ) {
+						System.out.println("Call Scheduler corto plazo");
+						reloj=reloj+ti;
+					}
 				
 				}
+				System.out.printf("PID=%s, Reloj=%.2f %n",lista.get(i).getPID(),reloj);
+				pant=lista.get(i).getPID();
 			}
-		
-			
-		
 		}
 	}
+	
+	
 	
 	private int restan() {
 		int r=0;
@@ -83,6 +94,27 @@ public class SPF {
 			p.reset();
 		}
 	}
+	
+	public String calcularT() {
+		procesar();
+		float TRP,TR;
+		float TEP,TE;
+		TEP=0;
+		TRP=0;
+		for(int i=0;i<lista.size();i++) {
+			TR=lista.get(i).getTR();
+			TE=TR-lista.get(i).getTS();
+			System.out.printf("PID=%s TS=%d TR=%.2f TE=%.2f  %n",lista.get(i).getPID(),lista.get(i).getTS(),TR,TE);
+			TEP+=TE;
+			TRP+=TR;
+		}
+		TEP=TEP/lista.size();
+		TRP=TRP/lista.size();
+		String s = "";
+		s = s.format("SPF TI=%.2f TRP=%.2f TEP=%.2f %n", ti, TRP, TEP);
+		return s;
+	}
+	
 	
 	
 	
