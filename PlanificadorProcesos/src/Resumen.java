6f8acbe0e85996c65a0acf75d34576cab925a434
esteban <eviani94@hotmail.com>
2022-09-16 22:43:52 -0300

    Agregando clase para resumen

---
+++ PlanificadorProcesos/src/Resumen.java
@@ -0,0 +1,36 @@
+
+public class Resumen {
+
+	private char PID;
+	private int TS;
+	private float TR;
+	private float TE;
+	
+	public char getPID() {
+		return PID;
+	}
+	public void setPID(char pID) {
+		PID = pID;
+	}
+	public int getTS() {
+		return TS;
+	}
+	public void setTS(int tS) {
+		TS = tS;
+	}
+	public float getTR() {
+		return TR;
+	}
+	public void setTR(float tR) {
+		TR = tR;
+	}
+	public float getTE() {
+		return TE;
+	}
+	public void setTE(float tE) {
+		TE = tE;
+	}
+	
+	
+	
+}
